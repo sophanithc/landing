bb7326498ac5efa094132e45c14ae1199213269a
Chea Sophanith <sophanithc@gmail.com>
2017-03-31 17:43:53 +0900

    Updated sidebar plugin

--- js/bsPlugin.js
+++ js/bsPlugin.js
@@ -1,91 +1,96 @@
 (function($){
   $.fn.CollapseSidebar = function(options, callback){
-    var me = this;
-    var className ="sidebar-nav";
-    var $el;
-    var configOptions = function(opt){
-      switch (opt){
-        case "show":
-          this.addClass("active");
-          break;
-        case "hide":
-          this.removeClass("active");
-          break;
-        case "collapse-sidebar":
-          console.log("I'm an enginner");          
-          break;
-        default:
-          console.warn("CollpaseSidebar: Invalid option");
-      }
-    }
-    var config = function(opt, val){
-      switch (opt){
-        case "sidebar":
-          if(val !== "right"){
-            me.removeClass("sidebar-right");
-            me.addClass("sidebar-left");
-          }
-          else{
-            me.removeClass("sidebar-left");
-            me.addClass("sidebar-right");
-          }
-          break;
-        case "width":
-          me.css(opt, val + "px");
-          break;
-        case "show":
-          if(val === true){
-            me.addClass("active");
-          }else{
-            me.removeClass("active");
-          }
-          break;
-        default:
-          console.warn("CollpaseSidebar: Invalid option");
-      }
+    var $me = this;
+    var sidebarClass ="sidebar-nav";
+    var actionClass ="sidebar-nav-toggler";
+    var $nav; 
 
-    }
-
-    var onclick = function(e){
+    var onNodeclick = function(e){
       e.preventDefault();
-      var $dom;
-      if(!me.data() || !me.data().target){
-        return;
+
+      if($nav.hasClass("active")){
+        $nav.removeClass("active");
+        $nav.css("left", 0);
+      }else{
+        $nav.addClass("active");
+        $nav.css("left", "-" + $nav.css("width"));
       }
-      $dom = $(me.data().target);
-      if(!$dom.length)
-        return;
-
-      $dom.toggleClass("active");
-
     }
 
     var bindEvents = function(){
-      me.on("click", onclick);
+      $me.on("click", onNodeclick);
+    }
+
+    var getSidebar = function(){
+      var $sidebarEl;
+      if($me.get(0).tagName.toLowerCase() == "a"){
+        $sidebarEl = $me.attr("href") ? $($me.attr("href")) : null;
+      }else{
+        $sidebarEl = ($me.data()  || $me.data().target) ? $($me.data().target) : null;
+      }
+
+      return ($sidebarEl && $sidebarEl.hasClass(sidebarClass)) ? $sidebarEl : null;
+    }
+
+    var configSingleOption = function(opt){
+      switch (opt){
+        case "show":
+          $nav.addClass("active");
+          break;
+        case "hide":
+          $nav.removeClass("active");
+          break;
+        default:
+          console.warn("CollpaseSidebar: Invalid option");
+      }
+    }
+    var configSettingOptions = function(settings){
+      for(var k in settings){
+        if(k == "sidebar"){
+          if(settings[k] === "right"){
+            $nav.addClass("sidebar-right");
+          }else{
+            $nav.removeClass("sidebar-right");
+          }
+        }else if(k == "show"){
+          if(settings[k] === true){
+            $nav.addClass("active");
+          }else{
+            $nav.removeClass("active");
+          }
+        }else if(k == "width"){
+          $nav.css(k,settings[k]) ;
+          $nav.css("left","-" + settings[k] + "px") ;
+        }
+      }
     }
 
     var init = function(){
+      if(!$me.hasClass(actionClass)){
+        return;
+      }
+
+      $nav = getSidebar();
+      if(!$nav || !$nav.length) return;
+
       bindEvents();
-      /*
+
       if(typeof options == "string"){
-        configOptions(options);
+        configSingleOptions(options);
       }else{
         var settings = $.extend({
-          sidebar:"left",
-          width:260,
+          sidebar:"left", // valid value: "left" and "right"
+          width:300,
           show:true
         }, options);
+        configSettingOptions(settings);
 
-        for (var key in settings){
-          config(key, settings[key]);
-        }
       }
-      */
     }
 
     init();
 
-    return this;
+    return $me;
 
   }
 }(jQuery));
